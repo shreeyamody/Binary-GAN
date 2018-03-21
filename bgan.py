aa5f0c49235249924f334cba1ccb3e0e2c6e746f
shreeyamody <modyshreeya@gmail.com>
2018-03-21 11:34:59 -0600

    made channel first

--- bgan.py
+++ bgan.py
@@ -31,62 +31,62 @@ def read_data():
 
 
 def Encoder(inputs):
-    c0 = tf.layers.conv2d(inputs = inputs,filters=64,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c0 = tf.layers.conv2d(inputs = inputs,filters=64,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv0")
 
-    c1 = tf.layers.conv2d(inputs = c0,filters=64,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c1 = tf.layers.conv2d(inputs = c0,filters=64,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv1")
 
-    mp0 = tf.layers.max_pooling2d(inputs = c1,pool_size = 2,strides = 2,padding='same',name="mp0")
+    mp0 = tf.layers.max_pooling2d(inputs = c1,pool_size = 2,strides = 2, data_format = 'channel_first', padding='same',name="mp0")
 
 
 
-    c2 = tf.layers.conv2d(inputs = mp0,filters=128,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c2 = tf.layers.conv2d(inputs = mp0,filters=128,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv2")
 
-    c3 = tf.layers.conv2d(inputs = c2,filters=128,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c3 = tf.layers.conv2d(inputs = c2,filters=128,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv3")
 
-    mp1 = tf.layers.max_pooling2d(inputs = c3,pool_size = 2,strides = 2,padding='same',name="mp1")
+    mp1 = tf.layers.max_pooling2d(inputs = c3,pool_size = 2,strides = 2, data_format = 'channel_first', padding='same',name="mp1")
 
 
 
-    c4 = tf.layers.conv2d(inputs = mp1,filters=256,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c4 = tf.layers.conv2d(inputs = mp1,filters=256,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv4")
 
-    c5 = tf.layers.conv2d(inputs = c4,filters=256,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c5 = tf.layers.conv2d(inputs = c4,filters=256,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv5")
 
-    c6 = tf.layers.conv2d(inputs = c45,filters=256,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c6 = tf.layers.conv2d(inputs = c45,filters=256,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv6")
 
-    mp2 = tf.layers.max_pooling2d(inputs = c6,pool_size = 2,strides = 2,padding='same',name="mp2")
+    mp2 = tf.layers.max_pooling2d(inputs = c6,pool_size = 2,strides = 2, data_format = 'channel_first', padding='same',name="mp2")
 
 
 
-    c7 = tf.layers.conv2d(inputs = mp2,filters=512,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c7 = tf.layers.conv2d(inputs = mp2,filters=512,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv7")
 
-    c8 = tf.layers.conv2d(inputs = c7,filters=512,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c8 = tf.layers.conv2d(inputs = c7,filters=512,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv8")
 
-    c9 = tf.layers.conv2d(inputs = c8,filters=512,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c9 = tf.layers.conv2d(inputs = c8,filters=512,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv9")
 
-    mp3 = tf.layers.max_pooling2d(inputs = c9,pool_size = 2,strides = 2,padding='same',name="mp3")
+    mp3 = tf.layers.max_pooling2d(inputs = c9,pool_size = 2,strides = 2, data_format = 'channel_first', padding='same',name="mp3")
 
 
 
-    c10 = tf.layers.conv2d(inputs = mp3,filters=512,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c10 = tf.layers.conv2d(inputs = mp3,filters=512,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv10")
 
-    c11 = tf.layers.conv2d(inputs = c10,filters=512,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c11 = tf.layers.conv2d(inputs = c10,filters=512,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv11")
 
-    c12 = tf.layers.conv2d(inputs = c11,filters=512,kernel_size=3,activation = tf.nn.relu, strides=(1,1), \
+    c12 = tf.layers.conv2d(inputs = c11,filters=512,kernel_size=3,activation = tf.nn.relu, strides=(1,1), data_format = 'channel_first',\
     kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),padding = "SAME",name = "conv12")
 
-    mp4 = tf.layers.max_pooling2d(inputs = c12,pool_size = 2,strides = 2,padding='same',name="mp4")
+    mp4 = tf.layers.max_pooling2d(inputs = c12,pool_size = 2,strides = 2, data_format = 'channel_first', padding='same',name="mp4")
 
 
 
@@ -164,4 +164,4 @@ with tf.Session() as sess:
 
     for e in range(epochs):
         num_batches = int(len(features)/batch_size)
-        optimizer = sess.run(generator_output,feed_dict = {X: features, Y: labels})
+        optimizer = sess.run(generator_output,feed_dict = {X: features})
